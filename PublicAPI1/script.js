b48c3d6533540ee58310073c70de18f787ae2b99
vivekkuma2610 <vivekpanneer2610@gmail.com>
2023-12-19 12:47:43 +0530

    update

--- PublicAPI1/script.js
+++ PublicAPI1/script.js
@@ -1,14 +1,26 @@
 
+import fetch from "node-fetch";
 
+fetch("https://emojihub.yurace.pro/api/random")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        let mypromise = new Promise((displayTable) => {
+            displayTable();
+        })
+        mypromise.then(function displayTable(data) {
+            var table = document.getElementById("emojiList").getElementsByTagName('tbody')[0];
+            var newRow = table.insertRow(table.length);
+            cell1 = newRow.insertCell(0);
+            cell1.innerHTML = data.name;
+            cell2 = newRow.insertCell(1);
+            cell2.innerHTML = data.category;
+            cell3 = newRow.insertCell(2);
+            cell3.innerHTML = data.group;
+            cell4 = newRow.insertCell(3);
+            cell4.innerHTML = data.htmlCode;
+            cell5 = newRow.insertCell(4);
+            cell5.innerHTML = data.unicode;
+        })
 
-async function loadTemperature(){
-    selectElement = document.querySelector('#animal');
-    animalName = selectElement.value;
-    console.log("animalName", animalName)
-    updatedname=animalName.toLowerCase();
-    let res1 = await fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=${updatedname}`)
-    let data1 = await res1.json()
-    console.log(data1)
-    console.log(`Fun fact about ${animalName} is ${data1.text}`)
-    document.getElementById("animalfactdisplay").innerHTML=`<b>Fun fact about ${animalName} is ${data1.text}<b/>`
-}
+    })
